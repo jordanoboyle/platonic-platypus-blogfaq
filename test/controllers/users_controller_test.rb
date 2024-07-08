5abfa3fe8e81f1e61f5cb6f1f12d21f92fc772d8
jordanoboyle <jmcmcode@gmail.com>
2024-07-07 18:19:58 -0600

    5 user seeds, user create test added

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "create" do
+    assert_difference "User.count", 1 do
+      post "/users.json", params: { first_name: "Test", last_name: "TEST", email: "test@test.com", username: "TEST2011", password: "password", password_confirmation: "password" }
+      assert_response 201
+    end
+  end
+
   # test "the truth" do
   #   assert true
   # end
