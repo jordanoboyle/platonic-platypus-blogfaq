d4a5c448ae36b57b0b3a6705870f73237f5b4538
jordanoboyle <jmcmcode@gmail.com>
2024-07-07 21:42:15 -0600

    on created a validation for updating password in the update action

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   
   validates :email, presence: true, uniqueness: true, on: :create
-  validates :password, presence: true, length: {minimum: 8}, on: :create
+  validates :password, presence: true, length: {minimum: 8}, on :create
 
   def update_with_password(params)
     if params[:password].blank?
