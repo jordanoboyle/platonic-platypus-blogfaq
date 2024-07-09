306bb6e187fa36fc9cf972e1811efe9d0ce3e156
jordanoboyle <jmcmcode@gmail.com>
2024-07-08 23:30:21 -0600

    genre create dynamic

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -11,10 +11,15 @@ class GenresController < ApplicationController
   end
 
   def create
-    @genre = Genre.create(
-      name: "Test Genre Name",
-      description: "This is a test description",
-      example: "Mario Clone"
+    @genre = Genre.new(
+      name: params[:name],
+      description: params[:description],
+      example: params[:example]
     )
+    if @genre.save
+      render template: "genres/show"
+    else
+      render json: { ERRORS: @genre.errors.fullmessages } 
+    end
   end
 end
